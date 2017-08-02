5d14feeff96529a2f42910b43b3df8a308c4745c
jzhugithub <mfx106825372@163.com>
2017-08-02 16:05:52 +0800

    add publish msg

--- src/multirobot_detect_iarc_node.cpp
+++ src/multirobot_detect_iarc_node.cpp
@@ -16,9 +16,119 @@
 #include "some_method.h"
 #include "parameter.h"
 
+//#include <dji_sdk/LocalPosition.h> //dji_sdk
+#include <multirobot_detect_iarc/RobotCamPos.h>
 
 using namespace cv;
 using namespace std;
+
+class PositionEstimate
+{
+public:
+  //node
+  ros::NodeHandle nh;
+  ros::NodeHandle nh_param;
+  //ros::Subscriber sub_loc;//dji_sdk
+  //robot and image parameter
+  int robot_x, robot_y, robot_width, robot_height, robot_center_x, robot_center_y;
+  double image_width, image_height;
+  //LocalPosition
+  bool listen_h_flag;
+  double loc_h;
+  //camera
+  double camera_pitch;
+  double fu,fv;
+  //estimate
+  float c2r_x;//camera 2 robot, camera frame
+  float c2r_y;
+  float gu, gv, pu, pv, lu, lv, alpha, yo, h;
+  double robot_mycam_x, robot_mycam_y;
+  
+  PositionEstimate():
+  nh_param("~")
+  {
+    //node
+    //sub_loc = nh.subscribe("/dji_sdk/local_position", 10, &PositionEstimate::localPositionCallback,this);//dji_sdk
+    //camera
+    if(!nh_param.getParam("camera_pitch", camera_pitch))camera_pitch = 36.0;
+    if(!nh_param.getParam("fu", fu))fu = 376.629954;
+    if(!nh_param.getParam("fv", fv))fv = 494.151786;
+    //LocalPosition
+    if(!nh_param.getParam("listen_h_flag", listen_h_flag))listen_h_flag = true;
+    if(!nh_param.getParam("loc_h", loc_h))loc_h = 1.5;
+  }
+  
+  ~PositionEstimate(){}
+  
+  void getEstimate(int robot_x0, int robot_y0, int robot_width0, int robot_height0, double image_width0, double image_height0)
+  {
+    //set robot and image parameter
+    robot_x = robot_x0;
+    robot_y = robot_y0;
+    robot_width = robot_width0;
+    robot_height = robot_height0;
+    image_width = image_width0;
+    image_height = image_height0;
+    robot_center_x = robot_x + robot_width / 2;
+    robot_center_y = robot_y + robot_height / 2;
+    
+    //runEstimate
+    if(camera_pitch>-90.0 && camera_pitch<30.0 && loc_h>0.5 && loc_h<4)
+    {
+      this->runEstimate();
+    }
+    else
+    {
+      cout<<"camera_pitch or loc_h is error"<<endl;
+      cout<<"camera_pitch: "<<camera_pitch<<endl;
+      cout<<"loc_h: "<<loc_h<<endl;
+    }
+  }
+  /*
+  void localPositionCallback(const dji_sdk::LocalPosition::ConstPtr& msg)//dji_sdk
+  {
+    if(listen_h_flag)
+      loc_h = msg->z;
+  }
+  */
+  
+private:
+  void runEstimate()
+  {
+    //c2r_x
+    gv = image_height / 2.0;
+    pv = robot_center_y;
+    yo = camera_pitch / 360.0 * (2.0*M_PI);
+    h = loc_h;
+    
+    alpha = atan((gv - pv) /fv);
+    c2r_x = tan(alpha + yo + M_PI/2.0) * h;
+    
+    //c2r_y
+    lu = robot_center_x;
+    pu = image_width / 2.0;
+    
+    c2r_y = (lu-pu)*cos(alpha)*h / (fu*cos(alpha+yo+M_PI/2.0));
+    /*
+    cout<<"gv: "<<gv<<endl;
+    cout<<"pv: "<<pv<<endl;
+    cout<<"yo: "<<yo<<endl;
+    cout<<"h: "<<h<<endl;
+    cout<<"(gv - pv) /fv: "<<(gv - pv) /fv<<endl;
+    cout<<"alpha: "<<alpha<<endl;
+    cout<<"c2r_x: "<<c2r_x<<endl;
+    cout<<"lu: "<<lu<<endl;
+    cout<<"pu: "<<pu<<endl;
+    cout<<"(lu-pu)*cos(alpha)*h: "<<(lu-pu)*cos(alpha)*h<<endl;
+    cout<<"(fu*cos(alpha+yo)): "<<(fu*cos(alpha+yo))<<endl;
+    cout<<"c2r_y: "<<c2r_y<<endl;
+    */
+    //publish
+    robot_mycam_x = c2r_x;
+    robot_mycam_y = c2r_y;
+  }
+};
+
 
 class MultirobotDetect
 {
@@ -29,6 +139,12 @@ public:
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   string subscribed_topic;
+  ros::Publisher msg_pub;
+  //PositionEstimate
+  bool publish_msg_flag;
+  PositionEstimate pe;
+  multirobot_detect_iarc::RobotCamPos rcp;
+  int rob_pub_num, obs_pub_num;
   //svm
   MySVM svm_detect;
   MySVM svm_classify;
@@ -64,7 +180,7 @@ public:
   vector<float> result_classify;
   bool save_set_flag;
   
-  MultirobotDetect():
+  MultirobotDetect(PositionEstimate pe0):
   it_(nh_),//intial it_
   nh_image_param("~")
   {
@@ -72,6 +188,12 @@ public:
     if(!nh_image_param.getParam("subscribed_topic", subscribed_topic))subscribed_topic = "/dji_sdk/image_raw";
     // Subscrive to input video feed from "/dji_sdk/image_raw" topic, imageCb is the callback function
     image_sub_ = it_.subscribe(subscribed_topic, 1, &MultirobotDetect::imageCb, this);
+    msg_pub  = nh_.advertise<multirobot_detect_iarc::RobotCamPos>("/robot_cam_position", 10);
+    //PositionEstimate
+    pe = pe0;
+    if(!nh_image_param.getParam("publish_msg_flag", publish_msg_flag))publish_msg_flag = false;
+    rob_pub_num = sizeof(rcp.rob_cam_pos_x) / sizeof(rcp.rob_cam_pos_x[0]);
+    obs_pub_num = sizeof(rcp.obs_cam_pos_x) / sizeof(rcp.obs_cam_pos_x[0]);
     //svm
     svm_detect.load(DetectSvmName);
     svm_classify.load(ClassifySvmName);
@@ -158,6 +280,9 @@ public:
     //cvtColor(src_3,src_4,CV_BGR2BGRA);//gpu
     //src_GPU.upload(src_4);//gpu
     
+    //reset
+    resetState();
+    
     //detect
     //HOG_descriptor_detect.detectMultiScale(src_GPU, location_detect, HitThreshold, WinStride, Size(), DetScale, 2);//gpu
     HOG_descriptor_detect.detectMultiScale(src_3, location_detect, HitThreshold, WinStride, Size(), DetScale, 2);
@@ -175,6 +300,7 @@ public:
       for(int j=0; j<descriptor_dim_classify; j++)  
 	descriptor_mat_classify.at<float>(0,j) = descriptor_classify[j];
       float temp_result_classify = svm_classify.predict(descriptor_mat_classify);
+      cout<<temp_result_classify;
       result_classify.push_back(temp_result_classify);
       
       //label the robot for (save video, show video, save set)
@@ -222,8 +348,35 @@ public:
 	}
       }
     }
-    location_detect.clear();
-    result_classify.clear();
+    
+    //publish msg
+    if(publish_msg_flag)
+    {
+      cout <<"publish"<<endl;
+      //set RobotCamPos
+      for(int i=0; i<result_classify.size(); i++)
+      {
+	if (result_classify[i] == 1 && rcp.rob_num < rob_pub_num)//irobot
+	{
+	  rcp.exist_rob_flag = true;
+	  pe.getEstimate(location_detect[i].x, location_detect[i].y, location_detect[i].width, location_detect[i].height, double(src_3.cols), double(src_3.rows));
+	  rcp.rob_cam_pos_x[rcp.rob_num] = pe.robot_mycam_x;
+	  rcp.rob_cam_pos_y[rcp.rob_num] = pe.robot_mycam_y;
+	  rcp.rob_num++;
+	}
+	else if (result_classify[i] == 2 && rcp.obs_num < obs_pub_num)//obstacle
+	{
+	  rcp.exist_obs_flag = true;
+	  pe.getEstimate(location_detect[i].x, location_detect[i].y, location_detect[i].width, location_detect[i].height, double(src_3.cols), double(src_3.rows));
+	  rcp.obs_cam_pos_x[rcp.obs_num] = pe.robot_mycam_x;
+	  rcp.obs_cam_pos_y[rcp.obs_num] = pe.robot_mycam_y;
+	  rcp.obs_num ++;
+	}
+      }
+      
+      //publish
+      msg_pub.publish(rcp);
+    }
     
     //save and show video
     if(save_result_video_flag)
@@ -237,17 +390,41 @@ public:
       waitKey(1);
     }
   }
+  
+  void resetState()
+  { 
+    //PositionEstimate
+    rcp.exist_rob_flag = false;
+    rcp.exist_obs_flag = false;
+    rcp.rob_num = 0;
+    rcp.obs_num = 0;
+    for(int i = 0; i < rob_pub_num; i++)
+    {
+      rcp.rob_cam_pos_x[i] = 0;
+      rcp.rob_cam_pos_y[i] = 0;
+    }
+    for(int i = 0; i < obs_pub_num; i++)
+    {
+      rcp.obs_cam_pos_x[i] = 0;
+      rcp.obs_cam_pos_y[i] = 0;
+    }
+    
+    //detect
+    location_detect.clear();
+    result_classify.clear();
+  }
 };
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "multirobot_detect_iarc_node");//node name
   double loop_rate;
-  MultirobotDetect md;//class initializing
+  PositionEstimate pe;//class initializing
+  MultirobotDetect md(pe);
   ros::NodeHandle nh_loop_param("~");
   if(!nh_loop_param.getParam("rate", loop_rate))loop_rate = 5;//video
   ros::Rate loop_rate_class(loop_rate);//frequency: n Hz
-  
+
   while(ros::ok())
     {
       ros::spinOnce();
